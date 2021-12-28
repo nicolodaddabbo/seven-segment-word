18f50cfa2239d14a6f594c083b2a9406acaacdd7
nicolodaddabbo <nicodaddabbo12@gmail.com>
2021-12-28 14:39:29 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/* 
+    What's the longest English word you can write on a seven-segment display? 
+    
+    Author: Nicolò D'Addabbo
+            nicolodaddabbo@gmail.com
+    
+    From an idea by Tom Scott
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\n");
+    return 0;
+}
